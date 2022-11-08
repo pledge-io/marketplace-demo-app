e38c782f34623557cced169124b0c3697a110699
Bertrand <bertrand.camara@yourpledge.earth>
2022-11-08 12:05:41 +0000

    Adjust tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     './src/views/**/*.ejs'
   ],
   theme: {
+    container: {
+      center: true
+    },
+    colors: {
+      white: '#fff',
+      blue: '#0569FF',
+      gray: {
+        200: '#D2D1CE'
+      }
+    },
     extend: {}
   },
   plugins: []
