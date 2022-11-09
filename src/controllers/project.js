bcc1b12c214c7ce8edf2d808fd511e3351eb1979
Bertrand <bertrand.camara@yourpledge.earth>
2022-11-09 10:24:16 +0000

    Project page

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -1,3 +1,15 @@
-export function projectController (req, res, next) {
-  res.render('pages/project', { project: {} })
+import { pledgeApi } from '../services/pledge'
+
+const { PLEDGE_API_URL } = process.env
+
+export async function projectController (req, res, next) {
+  try {
+    const response = await pledgeApi.get(`${PLEDGE_API_URL}/projects/${req.params.projectId}`)
+    const project = response.data
+
+    console.log(project)
+    res.render('pages/project', { project: { ...project, removal: true, avoidance: true } })
+  } catch (error) {
+    next(error)
+  }
 }
--- src/routes/project.js
+++ src/routes/project.js
@@ -3,6 +3,6 @@ import { projectController } from '../controllers'
 const router = express.Router()
 
 /* GET project */
-router.get('/project', projectController)
+router.get('/projects/:projectId', projectController)
 
 export default router
