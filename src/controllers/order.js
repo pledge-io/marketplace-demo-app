104dd55f5cfe4287639cfb5b8efa53d14fae6f24
Bertrand <bertrand.camara@yourpledge.earth>
2022-11-09 11:10:30 +0000

    Orders page

--- src/controllers/order.js
+++ src/controllers/order.js
@@ -1,3 +1,15 @@
-export function ordersController (req, res, next) {
-  res.render('pages/orders', { orders: [] })
+import { pledgeApi } from '../services/pledge'
+
+const { PLEDGE_API_URL } = process.env
+
+export async function ordersController (req, res, next) {
+  const response = await pledgeApi.get(`${PLEDGE_API_URL}/orders`, {
+    params: {
+      limit: 7,
+      offset: 0
+    }
+  })
+  const orders = response.data.data
+
+  res.render('pages/orders', { orders })
 }
