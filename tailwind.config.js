1f8d32551bda8c96554bad9875d7e173b35e0683
Bertrand <bertrand.camara@yourpledge.earth>
2022-11-09 11:10:04 +0000

    Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,22 @@ module.exports = {
       cornflower: '#D4F8FB',
       gray: {
         100: '#F9F9F9',
-        200: '#D2D1CE'
+        200: '#D2D1CE',
+        500: '#6D6D6D',
+      },
+      green: {
+        100: '#E2FBE8',
+        500: '#3C7E44'
+      },
+      yellow: {
+        100: '#FCF3CC'
+      },
+      brown: {
+        100: '#A85923'
+      },
+      red: {
+        100: '#F9E3E2',
+        500: '#AA2E26'
       }
     },
     extend: {}
